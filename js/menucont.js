967c4235ec8335f387b99f19dd1a317eebabcae6
danyerbert <danyerbert@gmail.com>
2022-01-04 14:43:06 -0400

    Página para Venezuela

---
+++ js/menucont.js
@@ -0,0 +1,19 @@
+const menu_icons = document.querySelector(".menu");
+const menu = document.querySelector(".menu-navegacion");
+
+
+//console.log(menu)
+//console.log(menu_icons)
+
+
+menu_icons.addEventListener("click", ()=>{
+    menu.classList.toggle("spread")
+})
+
+
+window.addEventListener("click", e=>{
+    if(menu.classList.contains("spread")
+      && e.target != menu && e.target != menu_icons){
+        menu.classList.toggle("spread")
+      }    
+})           
